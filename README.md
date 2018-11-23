7aa806c11fbedc65fa75844164648999c192fa59
Matt Izoulet <matt.izoulet@xing.com>
2018-11-23 14:56:31 +0100

    1rst cut

---
+++ README.md
@@ -0,0 +1,88 @@
+# Github Comment Badges
+
+> Beware: You are going to see your colleagues' faces... even more."
+
+Github is **awesome**... and yet sometimes you wish for a cooler, more effective, UX.
+Maybe you are used to look at long lists of pull requests of your team, periodically hitting the F5 key, to see if someone added a comment to a PR you are following.
+
+Well, good news, you shall no longer have to put up with this.
+
+This userscript will display a **colorful little indicators (also called 'badges')** in each row, in lists such as pull requests and issues.
+
+Doesn't it look better like this ?!
+
+<img src="images/comment-badges-overview.png" width="800">
+
+## How does it work ?
+
+The script will remember when you check a pull request (when you go to the details page), and the number of comments present at this moment.  
+It will then compare this count to the current count of comments of this PR.
+
+<table>
+<tr>
+<td><img src="images/badges.png"/></td>
+<td><ul>
+    <li>Gray badge: No new comments since last time you checked this PR</li>
+     <li>Blue badge: At least one new comment</li>
+     <li>Red badge: A lot of new comments (5 or more)</li>
+     <li>Faded text: You have never checked this PR... do it ! ;)</li>
+ </td>
+</tr>
+
+</table>
+
+Note the script <strong>peridiocally fetches the server data and updates the page without a full page refresh </strong>! 🔥  
+You will not need to hit F5 again to be up-to-date ️️✌️
+
+## Getting Started
+
+### What is a userscript ?
+
+**Userscripts** are little scripts injected in web pages to improve them. See them as addons, or extensions, (usually) scoped to pages or sites.  
+Adding features/information, changing UI, hiding undesired ads, are typical examples of what they usually do.
+
+Userscripts can easily be converted to browser extensions, or can be run by a userscript manager.
+
+### Prerequisite
+
+The application is has been tested/styled on the latest version of Chrome. (It should also work with Firefox)
+
+In order for your browser to run userscripts, you need to install a userscript manager extension. I recommand **Tampermonkey**  
+Install the [Tampermonkey extension](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo)
+
+### Installation
+
+Go to this url: [https://github.com/matthizou/github-show-avatars/raw/master/main.user.js](https://github.com/matthizou/github-comment-badges/raw/master/github-comment-badges.user.js)  
+Tampermonkey will pick up the fact that you are displaying a raw userscript and will ask you if you want to install the script.  
+Read the code (if you want) and click the **install** button.
+
+<img src="images/userscript-installation.png" width="600">
+
+### Github Enterprise
+
+At the moment, this script will work with any version of Github enterprise (tested on v2.14)
+
+Simply add a `@match` rule your domain at the beginning of the script, in the metadata block (you will need to edit it Tampermonkey)
+
+i.e, here at Xing I added:  
+`// @match https://source.xing.com/*`
+
+<p style="background-color:#BBB; color:black; border-radius: 3px; padding: 6px">
+💣 &nbsp Those manual modifications of the script will be overwritten everytime the script auto-update.   
+ Alternatively, you can create a new userscript in Tampermonkey and copy/paste the code there. But this method comes with issues, too: you won't benefit from the code updates, that often are cause to react to  breaking changes in the Github.<br>     
+ I plan to fork it in the future the scripts for Github and Github Enterprise, and provide for a solution to the problem above.
+</p>
+
+## Authors
+
+- **Matthieu Izoulet**
+
+## Acknowledgments
+
+Thanks to [Xing](https://www.xing.com/) for encouraging the development of this script. Hack weeks are awesome !
+
+## More
+
+I strongly recommend to also check the [Github Show Avatar userscript](https://github.com/matthizou/github-show-avatars). Look how great the two userscripts look, working in tandem.
+
+<img src="images/combined-avatars-and-comment-badges.png" width="800">
---
+++ github-comment-badges.user.js
@@ -0,0 +1,395 @@
+// ==UserScript==
+// @name         Github comment badges
+// @namespace    https://github.com/matthizou
+// @version      1.0
+// @description  Add badges to comment icons in PR list. Periodically and transparently refreshes those badges
+// @author       Matt
+// @match        https://github.com/*
+// @match        https://source.xing.com/*
+// @grant        GM.getValue
+// @grant        GM.setValue
+// @grant        GM.xmlHttpRequest
+// @grant        GM.info
+
+// ==/UserScript==
+
+;(async function() {
+  'use strict'
+  console.log('Starting extension: Github comment Badges')
+
+  const REFRESH_INTERVAL_PERIOD = 60000
+  let refreshIntervalId
+
+  // -------------------
+  // MAIN LOGIC FUNCTIONS
+  // -------------------
+
+  /** Fetch counts info from the server and update the list page */
+  function fetchCountData() {
+    console.log('fetchCountData')
+    const url = window.location.href
+    GM.xmlHttpRequest({
+      method: 'GET',
+      url,
+      onload: response => {
+        const newCountData = parseListPageHTML(response.responseText)
+        processListPage(newCountData)
+      },
+    })
+  }
+
+  /**
+   * Extract the count data from the HTML string representation of the page
+   * Notes: We may, in the future use the Github Rest/GraphQL Api to get those counts.
+   * It comes with its own overheads (such as providing an authentication token, instable API, etc.),
+   * and I find that for non-intensive queries such as here, scraping the HTML is more versatile and robust.
+   * */
+  function parseListPageHTML(pageHtml) {
+    let rowsInfo = [],
+      startSearchIndex,
+      rowHtml,
+      rowInfo,
+      comment,
+      htmlLength,
+      results = {}
+
+    const rowRegex = /id="issue_([0-9]+)/g
+    const commentRegex = /aria-label="([0-9]+) comment[s]?"/
+
+    // Find start of PR rows
+    let match = rowRegex.exec(pageHtml)
+    while (match !== null) {
+      rowsInfo.push({ id: match[1], index: match.index })
+      match = rowRegex.exec(pageHtml)
+    }
+
+    // Get count
+    for (let i = 0; i < rowsInfo.length; i++) {
+      rowInfo = rowsInfo[i]
+      startSearchIndex = rowInfo.index
+      htmlLength =
+        i === rowsInfo.length - 1
+          ? undefined
+          : rowsInfo[i + 1].index - startSearchIndex
+      rowHtml = pageHtml.substr(startSearchIndex, htmlLength)
+      comment = commentRegex.exec(rowHtml)
+      results[rowInfo.id] = comment ? parseInt(comment[1], 10) : 0
+    }
+
+    return results
+  }
+
+  /**
+   * Processing function for the list pages.
+   * Compare displayed count data to stored count data, adding badges and extra styling to the comments container of each row.
+   * @params {Object} fetchedData - Optional. Fresh data from the server.
+   */
+  async function processListPage(fetchedData) {
+    const repoData = await getRepoData()
+    const dataToUpdate = {}
+
+    $('.repository-content [data-id]').forEach(row => {
+      const pullRequestId = row.id.replace('issue_', '')
+      const icon = row.querySelector('.octicon-comment')
+
+      let container, displayedMessageCount
+      if (icon) {
+        container = icon.parentNode
+        displayedMessageCount = parseInt(container.innerText, 10) // todo : WEAK. Use aria-label instead
+      } else {
+        container = row.querySelector('.float-right .float-right')
+        displayedMessageCount = 0
+      }
+
+      const countFromServer =
+        fetchedData && fetchedData[pullRequestId] >= 0
+          ? fetchedData[pullRequestId]
+          : undefined
+      const messageCount =
+        countFromServer !== undefined ? countFromServer : displayedMessageCount
+
+      if (displayedMessageCount !== messageCount) {
+        // The displayed count is outdated, update it
+        if (messageCount === 0) {
+          // The count has decreased to 0, remove icon
+          container.innerHTML = ''
+        } else if (displayedMessageCount === 0) {
+          // We need to add the icon
+          container.innerHTML = `
+                          <a href="/facebook/react/pull/14301" class="muted-link" aria-label="${messageCount} comments" style="position: relative;">
+                            <svg class="octicon octicon-comment v-align-middle" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M14 1H2c-.55 0-1 .45-1 1v8c0 .55.45 1 1 1h2v3.5L7.5 11H14c.55 0 1-.45 1-1V2c0-.55-.45-1-1-1zm0 9H7l-2 2v-2H2V2h12v8z"></path></svg><i data-id="unread-notification" style="position: absolute; z-index: 2; border-radius: 50%; color: rgb(255, 255, 255); width: 8px; height: 8px; top: 0px; left: 9px; border-width: 0px; background-image: linear-gradient(rgb(187, 187, 187), rgb(204, 204, 204));"></i>
+                            <span class="text-small text-bold">${messageCount}</span>
+                          </a>
+                      `
+        } else {
+          // The icon exists - simply update text
+          container.querySelector('span').innerText = messageCount // WEAK
+        }
+      }
+
+      const storedMessageCount = repoData[pullRequestId]
+
+      if (storedMessageCount && messageCount < displayedMessageCount) {
+        // The count decreased
+        // Stored data needs to be updated in certain cases, otherwise an new incoming comment may not be notified
+        dataToUpdate[pullRequestId] = messageCount
+      }
+
+      if (storedMessageCount === undefined) {
+        // The user has never looked at this PR
+        if (container) {
+          toggleUnreadStyle(container, true)
+        }
+      } else if (messageCount > storedMessageCount) {
+        // This PR has new messages
+        showMessageNotificationIcon({
+          container,
+          hasUnread: true,
+          highlight: messageCount - storedMessageCount >= 5,
+        })
+      } else if (messageCount > 0) {
+        // This PR has no new messages
+        showMessageNotificationIcon({
+          container,
+          hasUnread: false,
+        })
+      }
+    })
+
+    if (Object.keys(dataToUpdate).length) {
+      setRepoData({ ...repoData, ...dataToUpdate })
+    }
+  }
+
+  /**
+   * Processing function for the detail pages.
+   * Looks for the count number and stores it
+   */
+  async function processDetailsPage() {
+    console.log('processDetailsPage')
+    const repoData = await getRepoData()
+    const pullRequestId = /pull\/([0-9]+)/.exec(window.location.pathname)[1]
+    const messageCount = parseInt(
+      document.querySelector('#conversation_tab_counter').innerText,
+      10,
+    )
+
+    // Compare current number of messages in the PR to the one stored from the last visit
+    // Update it if they don't match
+    if (Number.isInteger(messageCount)) {
+      const previousMessageCount = repoData[pullRequestId]
+      if (messageCount !== previousMessageCount) {
+        setRepoData({ ...repoData, [pullRequestId]: messageCount })
+      }
+    }
+  }
+
+  const selectorEnum = {
+    LIST: '#js-issues-toolbar',
+    DETAILS: '#discussion_bucket',
+  }
+
+  async function applyExtension() {
+    const { repoOwner, repo, section } = getInfoFromUrl()
+
+    // Element that signals that we are on such or such page
+    let landmarkElement
+    if (section === 'pulls') {
+      landmarkElement = await waitForUnmarkedElement(selectorEnum.LIST)
+      markElement(landmarkElement)
+      processListPage()
+      if (!refreshIntervalId) {
+        refreshIntervalId = setInterval(fetchCountData, REFRESH_INTERVAL_PERIOD)
+      }
+    } else if (section === 'pull') {
+      landmarkElement = await waitForUnmarkedElement(selectorEnum.DETAILS, {
+        priority: 'low',
+      })
+      markElement(landmarkElement)
+      processDetailsPage()
+      clearInterval(refreshIntervalId)
+      refreshIntervalId = null
+    } else {
+      clearInterval(refreshIntervalId)
+      refreshIntervalId = null
+    }
+  }
+
+  function toggleUnreadStyle(container, isUnread) {
+    const unreadColor = '#c6cad0'
+    if (isUnread) {
+      container.style.setProperty('color', unreadColor, 'important')
+    } else {
+      container.style.removeProperty('color')
+    }
+  }
+
+  function showMessageNotificationIcon({ container, hasUnread, highlight }) {
+    const icon = container && container.querySelector('svg')
+    if (!icon) {
+      return
+    }
+    let notification = container.querySelector(
+      '[data-id="unread-notification"]',
+    )
+    if (!notification) {
+      container.style.position = 'relative'
+      notification = document.createElement('i')
+      notification.dataset.id = 'unread-notification'
+      notification.style.position = 'absolute'
+      notification.style.zIndex = 2
+      notification.style.borderRadius = '50%'
+      notification.style.color = '#fff'
+      notification.style.width = '8px'
+      notification.style.height = '8px'
+      notification.style.top = '0px'
+      notification.style.left = '9px'
+      notification.style.borderWidth = 0
+
+      insertAfter(notification, icon)
+    }
+    if (hasUnread) {
+      notification.style.backgroundImage = highlight
+        ? 'linear-gradient(#d73a49, #cb2431)' // Red/orange
+        : 'linear-gradient(#54a3ff,#006eed)' // Blue
+    } else {
+      notification.style.backgroundImage = 'linear-gradient(#BBB,#CCC)' // Grey
+    }
+
+    toggleUnreadStyle(container, false)
+  }
+
+  /** Check page url and returns whether or not we are in a list page (pull request/issues lists )*/
+  function isListPage() {
+    const { section } = getInfoFromUrl()
+    return section === 'pulls' || section === 'issues'
+  }
+
+  async function getRepoData() {
+    const key = getDataKey()
+    return await getStoreData(key)
+  }
+
+  async function setRepoData(data) {
+    const key = getDataKey()
+    return await GM.setValue(key, data)
+  }
+
+  function getDataKey() {
+    const { repoOwner, repo } = getInfoFromUrl()
+    return `${repoOwner}/${repo}`
+  }
+
+  const PROCESSED_FLAG = GM.info.script.uuid
+
+  function markElement(element) {
+    element.dataset[PROCESSED_FLAG] = true
+  }
+
+  function isMarked(element) {
+    return element.dataset[PROCESSED_FLAG]
+  }
+
+  async function waitForUnmarkedElement(selector, options = {}) {
+    return await waitFor(selector, {
+      ...options,
+      condition: element => !isMarked(element),
+    })
+  }
+
+  // -------------------
+  // STARTUP BLOCK
+  // -------------------
+
+  // Process page
+  applyExtension()
+
+  // Ensure we rerun the page transform code when the route changes
+  const pushState = history.pushState
+  history.pushState = function() {
+    pushState.apply(history, arguments)
+    applyExtension()
+  }
+
+  // Handle browser navigation changes (previous/forward button)
+  window.onpopstate = function(event) {
+    if (isListPage()) {
+      fetchCountData()
+      refreshIntervalId = setInterval(fetchCountData, REFRESH_INTERVAL_PERIOD)
+    }
+  }
+
+  // ---------------
+  // UTIL FUNCTIONS
+  // ---------------
+
+  /**
+   * Extract the username of the repo owner from the url
+   * i.e: https://github.com/styleguidist/react-styleguidist/pulls ====> styleguidist
+   */
+  function getInfoFromUrl() {
+    const [repoOwner, repo, section] = window.location.pathname
+      .substr(1)
+      .split('/')
+    return {
+      repoOwner,
+      repo,
+      section,
+    }
+  }
+
+  /** Get data from data store */
+  async function getStoreData(namespace) {
+    const data = await GM.getValue(namespace)
+    return data || {}
+  }
+
+  /** Shorthand for querySelectorAll, JQuery style */
+  function $(selector, element = document) {
+    return Array.from(element.querySelectorAll(selector))
+  }
+
+  /** Insert in DOM the specified node right after the specified reference node */
+  function insertAfter(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling)
+  }
+
+  /**
+   * Wait for an element to appear in document. When not found, wait a bit, and tries again,
+   * until the maximum waiting time is reached.
+   * @return {Promise}
+   */
+  function waitFor(selector, options = {}) {
+    const { priority = 'medium', condition, maxTime = 20000 } = options
+
+    let intervalPeriod
+    switch (priority) {
+      case 'low':
+        intervalPeriod = 500
+        break
+      case 'high':
+        intervalPeriod = 50
+        break
+      default:
+        intervalPeriod = 200
+        break
+    }
+    const maxRetries = Math.floor(maxTime / intervalPeriod)
+
+    let iterationCount = 0
+
+    return new Promise((resolve, reject) => {
+      const interval = setInterval(() => {
+        const element = document.querySelector(selector)
+        if (element && (!condition || condition(element))) {
+          clearInterval(interval)
+          resolve(element)
+        } else if (++iterationCount > maxRetries) {
+          // End of cycle with failure
+          clearInterval(interval)
+          reject("Github PR extension error: timeout, couldn't find element")
+        }
+      }, intervalPeriod)
+    })
+  }
+})()
